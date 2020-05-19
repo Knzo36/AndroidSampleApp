3526cf98513539f96f31378e3dadf334121ce5c2
KenzaburoTakagi <ken.takagi216@gmail.com>
2020-05-19 23:58:05 +0900

    list java code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ArrayList<String> data = new ArrayList<>();
+        data.add("ウホ");
+        data.add("ウホホ");
+        data.add("ウホホ");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, )
     }
 }
